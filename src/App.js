d8abad6dff09e7520318e0895c98a8bec7423e3f
Maksim <max-goryachev@ya.ru>
2023-09-25 15:03:38 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Component from './component';
+import { useState } from 'react';
+function App() {  
+  return (
+  <div>
+    
+  </div>
+  );
+  }
+
+export default App;
